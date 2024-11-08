23502aeebdfe208c0b3548dd0a8c9c92d4c14abb
haviet4012 <tranquocvietbn321@gmail.com>
2024-11-08 16:17:05 +0700

    all changes added

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -3,7 +3,7 @@ import Header from "@/components/Header";
 import ListItem from "@/components/ListItem";
 
 import PageContent from "./PageContent";
-export const reavalidate = 0;
+export const revalidate = 0;
 
 export default async function Home() {
   const songs = await getSongs();
